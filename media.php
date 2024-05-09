20662939675be3e2b8dc0a0d835ec4d0dea5159e
eyshkamilly <eyshilakamilly22@gmail.com>
2024-05-09 15:14:50 -0300

    Cálculo de média

---
+++ media.php
@@ -0,0 +1,28 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Média</title>
+</head>
+<body>
+
+    <h1> Cálculo da média dos alunos do 3°A do Sesi Moreno. </h1>
+
+    <?php
+
+        $n1 = 6;
+
+        $n2 = 8 ;
+        
+        $n3 = 10 ;
+
+        $media = ($n1+$n2+$n3) / 3 ; 
+
+        echo "A nota do aluno foi: $media";
+
+    
+    
+    ?>
+    
+</body>
+</html>
